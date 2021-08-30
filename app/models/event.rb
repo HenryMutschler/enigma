25108eef911f5592b1eed818a74d8c8d9f5ce602
Henry Mutschler <henryamutschler@gmail.com>
2021-08-30 17:09:42 +0100

    Added models for booking and its migration

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,11 @@
 class Event < ApplicationRecord
+    has_many :bookings
+    has_many :favourites
+    CATEGORIES = ["Vegetarian", "Mediterranean", "Turkish", "Tapas", "Vegan", "Italian", "French", "British", "German"]
+    validates :postcode, presence: true
+    validates :restaurant_name, presence: true
+    validates :category, inclusion: { in: CATEGORIES }
+    validates :event_name, presence: true
+    validates :event_descritpion, presence: true
+    validates :price, presence: true
 end
