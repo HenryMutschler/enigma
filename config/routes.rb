9a08d3a8e451d044679ad11d464ae1b53eccf8c7
Henry Mutschler <henryamutschler@gmail.com>
2021-08-31 11:10:46 +0100

    Booking Controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   
-  resources :users, only: %i[show]
+  resource :users, only: %i[show]
 
   resources :events do 
     resources :bookings, only: %i[new create]
