840c2c655a32dd7e68bce6e43b91bdac817522f5
Ivory Lau <ivory99142@gmail.com>
2021-08-31 12:03:59 +0100

    Added sign in/ user setting/ routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  
-  resources :users, only: %i[show]
 
-  resources :events do 
+  resources :favourites, only: %i[new create index destroy]
+
+  resources :events do
     resources :bookings, only: %i[new create]
   end
-
   resources :bookings, only: %i[index show edit update]
+
+  root to: 'pages#home'
 end
