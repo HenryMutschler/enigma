17dfe209049c601cda6c692fe406b36c3b290849
Mia Jimenez <miavjimenez28@gmail.com>
2021-09-01 12:26:49 +0100

    trying to fix error

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,9 @@ Rails.application.routes.draw do
 
   resources :events do
     resources :bookings, only: %i[new create]
-   # resources :favourites, only: %i[create destroy]
+    # resources :favourites, only: %i[create destroy]
   end
   resources :bookings, only: %i[index show edit update]
   resources :favourites, only: %i[index create destroy]
 
-  root to: 'pages#home'
 end
