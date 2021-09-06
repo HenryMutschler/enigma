1e53b32c31880d750006dca060e648f0e127d861
Ivory Lau <ivory99142@gmail.com>
2021-09-06 16:31:40 +0100

    added review seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-
   resources :users, only: %i[show]
-
 
   resources :events do
     resources :bookings, only: %i[new create]
-    # resources :favourites, only: %i[create destroy]
   end
   resources :bookings, only: %i[index show edit update]
-  resources :favourites, only: %i[index create destroy]
 
+  resources :favourites, only: %i[index create destroy]
+  resources :reviews, only: %i[index new create]
+
+  root to: 'pages#home'
 end
