e0583264716802e7a80b964655f6869937f7dfea
Ivory Lau <ivory99142@gmail.com>
2021-09-09 10:26:35 +0100

    added logic for reviews orderby created time on user show

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user # user
-  belongs_to :event
+  belongs_to :event # from organizer
   has_one :review
 
   validates :ticket_amount, presence: true
